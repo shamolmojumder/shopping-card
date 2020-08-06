e1feef298c9f83c6f97042bb666e0acd3b6ddb98
unknown <shamolmojumder14@gmail.com>
2020-08-06 21:31:23 +0600

    added

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 const singlePhonePrice = 1219;
-const singleCoverPrice =
-    calculation("phone_plus", singlePhonePrice)
+const singleCoverPrice = 59;
+// calculation("phone_plus", singlePhonePrice, "phone_value")
+    // calculation("cover_plus", singleCoverPrice)
 
-function calculation(id, singlePhonePrice) {
-    document.getElementById(id).addEventListener("click", function() {
-        const phone_value = document.getElementById("phone_value").value
-        const phone_valueNumber = parseFloat(phone_value)
-        const addPhone = phone_valueNumber + 1;
-        document.getElementById("phone_value").value = addPhone;
+// function calculation(id, singlePhonePrice, id) {
+//     document.getElementById(id).addEventListener("click", function() {
+//         const phone_value = document.getElementById(id).value
+//         const phone_valueNumber = parseFloat(phone_value)
+//         const addPhone = phone_valueNumber + 1;
+//         document.getElementById(id).value = addPhone;
 
-        const phonePrice = document.getElementById("phonePrice").innerText
-        const phonePriceNumber = parseFloat(phonePrice);
-        const addPhonePrice = phonePriceNumber + singlePhonePrice;
-        document.getElementById("phonePrice").innerText = addPhonePrice
+//         const phonePrice = document.getElementById("phonePrice").innerText
+//         const phonePriceNumber = parseFloat(phonePrice);
+//         const addPhonePrice = phonePriceNumber + singlePhonePrice;
+//         document.getElementById("phonePrice").innerText = addPhonePrice
 
-        const subTotal = document.getElementById("subTotal").innerText;
-        const subTotalNumber = parseFloat(subTotal)
-        const subTotalPrice = subTotalNumber + singlePhonePrice;
-        document.getElementById("subTotal").innerText = subTotalPrice;
-    })
-}
+//         const subTotal = document.getElementById("subTotal").innerText;
+//         const subTotalNumber = parseFloat(subTotal)
+//         const subTotalPrice = subTotalNumber + singlePhonePrice;
+//         document.getElementById("subTotal").innerText = subTotalPrice;
+//     })
+// }
 
-// document.getElementById("phone_plus").addEventListener("click", function() {
-//     const phone_value = document.getElementById("phone_value").value
-//     const phone_valueNumber = parseFloat(phone_value)
-//     const addPhone = phone_valueNumber + 1;
-//     document.getElementById("phone_value").value = addPhone;
+document.getElementById("phone_plus").addEventListener("click", function() {
+    const phone_value = document.getElementById("phone_value").value
+    const phone_valueNumber = parseFloat(phone_value)
+    const addPhone = phone_valueNumber + 1;
+    document.getElementById("phone_value").value = addPhone;
 
-//     const phonePrice = document.getElementById("phonePrice").innerText
-//     const phonePriceNumber = parseFloat(phonePrice);
-//     const addPhonePrice = phonePriceNumber + singlePhonePrice;
-//     document.getElementById("phonePrice").innerText = addPhonePrice
+    const phonePrice = document.getElementById("phonePrice").innerText
+    const phonePriceNumber = parseFloat(phonePrice);
+    const addPhonePrice = phonePriceNumber + singlePhonePrice;
+    document.getElementById("phonePrice").innerText = addPhonePrice
 
-//     const subTotal = document.getElementById("subTotal").innerText;
-//     const subTotalNumber = parseFloat(subTotal)
-//     const subTotalPrice = subTotalNumber + singlePhonePrice;
-//     document.getElementById("subTotal").innerText = subTotalPrice;
-// })
+    const subTotal = document.getElementById("subTotal").innerText;
+    const subTotalNumber = parseFloat(subTotal)
+    const subTotalPrice = subTotalNumber + singlePhonePrice;
+    document.getElementById("subTotal").innerText = subTotalPrice;
+})
